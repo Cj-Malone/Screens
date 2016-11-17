89dafa4a2df41a8dcb8eba61ad8631f9aea40a81
Cj Malone <Cj-Malone@users.noreply.github.com>
2016-11-17 20:55:52 +0000

    Inital Commit
    Started this yesterday, shame no way to progromatically launch
    splitscreen mode

---
+++ app/src/main/java/uk/cjmalone/screen/MainActivity.java
@@ -0,0 +1,41 @@
+package uk.cjmalone.screen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText package1EditText = (EditText) findViewById(R.id.package1EditText);
+        final EditText package2EditText = (EditText) findViewById(R.id.package2EditText);
+
+        final EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
+        final Button createShortcutButton = (Button) findViewById(R.id.createShortcutButton);
+
+        createShortcutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent installIntent = new Intent();
+                installIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+
+                installIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, (String) nameEditText.getText().toString());
+                installIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT,
+                        ShortcutActivity.createShortcutIntent(view.getContext(),
+                                package1EditText.getText().toString(),
+                                package2EditText.getText().toString()
+                        ));
+
+                view.getContext().sendBroadcast(installIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/uk/cjmalone/screen/ShortcutActivity.java
@@ -0,0 +1,73 @@
+package uk.cjmalone.screen;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class ShortcutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (isInMultiWindowMode()) {
+            thunderbirdsAreGo();
+        } else {
+            setContentView(R.layout.activity_shortcut);
+        }
+    }
+
+    public void thunderbirdsAreGo() {
+        //Seems to be an issue with passing arrays?
+
+        /* if (!getIntent().hasExtra("pkg")) {
+            Toast.makeText(this, "No Packages", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        Intent[] intents = new Intent[getIntent().getStringArrayListExtra("pkg").size()];
+        int i = 0;
+
+        for (String pkg : getIntent().getStringArrayListExtra("pkg")) {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(pkg);
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            intents[i] = intent;
+            i++;
+        }
+
+        startActivities((intents); */
+
+        Intent primaryIntent = getPackageManager().getLaunchIntentForPackage(getIntent().getStringExtra("pkg1"));
+        primaryIntent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
+        primaryIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        Intent secondaryIntent = getPackageManager().getLaunchIntentForPackage(getIntent().getStringExtra("pkg2"));
+        secondaryIntent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
+        secondaryIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivities(new Intent[]{primaryIntent, secondaryIntent});
+        finish();
+    }
+
+    public static Intent createShortcutIntent(Context context, String package1, String package2) {
+        Intent shortcutIntent = new Intent();
+        shortcutIntent.setClass(context, ShortcutActivity.class);
+
+        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+
+        shortcutIntent.putExtra("pkg1", package1);
+        shortcutIntent.putExtra("pkg2", package2);
+
+        /* shortcutIntent.putExtra("pkg", new String[]{
+                        package1,
+                        package2}); */
+
+        return shortcutIntent;
+    }
+}
